1bf4d6908544ee7ebdc0134a8fa7cd79da8095b7
Luis Mayo Valbuena <luismayovalbuena@outlook.es>
2021-05-20 15:17:21 +0200

    Added easier import

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import sys
 sys.path.append('objection_engine/')
 import objection_engine
+# You can also import the components like this
+from objection_engine.renderer import render_comment_list
+from objection_engine.beans.comment import Comment
 foo = [objection_engine.comment.Comment(), objection_engine.comment.Comment(text_content='Second comment',  user_name="Second user")]
 objection_engine.renderer.render_comment_list(foo)
